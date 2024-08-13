992096f0d69c7cfebb46474a10c67da77cbb779b
Fazle Rabbi <frduiit@gmail.com>
2024-08-13 19:15:03 -0400

    Update run_llama.sh

--- commands/run_llama.sh
+++ commands/run_llama.sh
@@ -1,4 +1,14 @@
 ## LLAMA
+
+#!/encs/bin/bash
+#SBATCH -J pass_finder --partition=pa --mem=16GB --gpus=1  --account=jinqiuy
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=l_ling20@encs.concordia.ca
+# Specify the output file name
+#SBATCH -o llama.log
+module load anaconda3/2023.03/default
+conda activate /speed-scratch/l_ling20/FairT
+
 bash exp_by_hyp_batch.sh llama 5 default
 bash exp_by_style_batch.sh llama 5 1.0
 bash exp_by_iterations.sh llama 3 5
