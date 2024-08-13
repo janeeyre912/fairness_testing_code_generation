e4f110703f7c1caca92d742adf7b08efec55886c
Fazle Rabbi <frduiit@gmail.com>
2024-08-13 19:15:59 -0400

    Update run_gpt.sh

--- commands/run_gpt.sh
+++ commands/run_gpt.sh
@@ -1,4 +1,14 @@
 ## GPT
+
+#!/encs/bin/bash
+#SBATCH -J gpt_job --partition=pa --mem=16GB --gpus=1  --account=jinqiuy
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=l_ling20@encs.concordia.ca
+# Specify the output file name
+#SBATCH -o gpt.log
+module load anaconda3/2023.03/default
+conda activate /speed-scratch/l_ling20/FairT
+
 bash exp_by_hyp_batch.sh gpt 5 default
 bash exp_by_style_batch.sh gpt 5 1.0
 bash exp_by_iterations.sh gpt 3 5
