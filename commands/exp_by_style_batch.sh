2c9d8f031645452a8ef0f634d3776fe7d9a21c1a
Fazle Rabbi <frduiit@gmail.com>
2024-08-14 13:07:32 -0400

    Update exp_by_style_batch.sh

--- commands/exp_by_style_batch.sh
+++ commands/exp_by_style_batch.sh
@@ -3,11 +3,10 @@ CURRENT_DIR=$(pwd)
 SAMPLING="10"
 TEMPERATURE="1.0"
 DATA_PATH="$CURRENT_DIR""/../dataset/prompts.jsonl"
-OUTPUT_PATH="$CURRENT_DIR""/../outputs/"
 MODEL_NAME=$1
 
-STYLE_PARTIAL="partial"
-DATA_PATH_PARTIAL="$CURRENT_DIR""/../dataset/prompts_32_partial.jsonl"
+#STYLE_PARTIAL="partial"
+#DATA_PATH_PARTIAL="../dataset/prompts_32_partial.jsonl"
 
 if [ "$#" -le 0 ]; then
     echo "Usage: bash exp_by_style_batch.sh [model_name]"
@@ -26,7 +25,7 @@ temperature="${TEMPERATURE/./}"
 
 for i in "default" "chain_of_thoughts" "positive_chain_of_thoughts"
 do
-    bash gen_and_exp.sh "$SAMPLING" "$TEMPERATURE" $i $DATA_PATH "$CURRENT_DIR""/../outputs/styles/""$MODEL_NAME""$temperature"$i "$MODEL_NAME"
+    bash gen_and_exp.sh "$SAMPLING" "$TEMPERATURE" $i $DATA_PATH "$OUTPUT_PATH""/../outputs/styles/""$MODEL_NAME""$temperature"$i "$MODEL_NAME"
 done
 
-#bash gen_and_exp.sh "$SAMPLING" "$TEMPERATURE" $STYLE_PARTIAL "$DATA_PATH_PARTIAL" DATA_PATH_PARTIAL"/../outputs/styles/""$MODEL_NAME""$temperature""$STYLE_PARTIAL" "$MODEL_NAME"
+#bash gen_and_exp.sh "$SAMPLING" "$TEMPERATURE" $STYLE_PARTIAL "$DATA_PATH_PARTIAL" "../outputs/styles/""$MODEL_NAME""$temperature""$STYLE_PARTIAL" "$MODEL_NAME"
